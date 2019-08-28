e5f6b5b7dcc67f486f68a1e206c4ee7531037487
CoBeCTb <tsop.tya@gmail.com>
2019-08-28 11:07:00 +0600

    * opt2 creation

--- [views]/catalog/opt2.php
+++ [views]/catalog/opt2.php
@@ -16,98 +16,74 @@ foreach($MODEL['catalog'] as $cat)
 
 //vd($arr);
 ?>
-111111111111111111
 <!--крамбсы-->
 <? Core::renderPartial(SHARED_VIEWS_DIR.'/crumbs.php', $MODEL['crumbs']);?>
 <!--//крамбсы-->
 
 
 
-<style>
-.tbl{text-align: center; width: 100%; }
-.tbl tr{}
-.tbl td, .tbl th{ border: 1px solid #000; /*padding: 5px 12px;*/ font-size: 12px;  text-align: center; padding: 7px 5px;  }
-.tbl th{white-space: nowrap; font-weight: normal; }
-.hdr{background: #0669b3; color: #fff; }
-.product-name{text-align: center !important; /*width: 280px !important;*/ font-weight: bold;   }
-.product-name a{font-size: 12px; }
-.doze{/*width: 160px;*/ text-align: center !important; }
-.doze a{text-decoration: none ; }
-.tbl .price{white-space: nowrap; min-width: 80px; height: 33px !important; vertical-align: middle;  }
-.tbl tr:hover{/*background: #eee;*/}
-</style>
-
-
-
-
-
 <div style="text-align: center; ">
-<div style="margin: 0 auto; width: 1000px; ">
+    <div style="margin: 0 auto; width: 1000px; ">
 
-	<?=$MODEL['textBefore']->attrs['descr']?>
+        <div class=" container1 opt">
 
-	<table  class="tbl" style="">
-		<tr height="176">
-			<td align="left" height="176" style="vertical-align: middle;  border-right: 0px solid red;"><img src="/img/logo.png" width="350" /></td>
-			<td style="text-align: center ; border-left: 0px solid red;">
-				<?=$MODEL['textTableTop']->attrs['descr']?>
-			</td>
-		</tr>
-	</table>
+            <div class="row limiter">
+                <?=$MODEL['textBefore']->attrs['descr']?>
+            </div>
 
-    <p></p>
-	<table class="tbl" width="">
-		<tr class="hdr">
-			<th>Наименование</th>
-			<th>Фасовка</th>
-			<th>Розница</th>
-			<?foreach(ProductSimple::$optPrices as $sum=>$isShown):?>
-				<?if(!$isShown || 1)continue;?>
-			    <th><?=Funx::numberFormat($sum)?> $</th>
-			<?endforeach?>
-		</tr>
-	<?foreach($MODEL['catalog'] as $cat):?>
-		<tr class="hdr">
-			<td colspan="<?=count(ProductSimple::$optPrices)+1?>"><?=$cat->name?></td>
-		</tr>
-		<?foreach($cat->products as $name=>$products):?>
-            <?
-			$count = count($products);
-			$i=0;
-			?>
-			
-			<?foreach($products as $product):?>
-                <?if(!$product->optPricesArr) continue;?>
-			<tr>
-				<?if($i==0):?>
-					<td class="product-name" rowspan="<?=$count?>">
-						<!-- <a href="/<?=UPLOAD_IMAGES_REL_DIR?><?=$product->photo?>" class="highslide" onclick="return hs.expand(this)" style="vertical-align: middle; "><img src="<?=$product->photo ? Media::img($product->photo) : Funx::noPhotoSrc()?>&width=50" alt="<?=$product->name?>" /></a> -->
-						<?=$product->name?>
-					</td>
-				<?endif;?>
-				
-				<td class="doze"><a href="<?=$product->url()?>" target="_blank" ><?=OptPrice::shortenDozeStr($product->inPackage)?></a></td>
-				
-				<td class="price"><?=Currency::drawAllCurrenciesPrice($product->price)?></td>
-				
-				<?foreach(ProductSimple::$optPrices as $sum=>$isShown):?>
-					<?if(!$isShown || 1)continue;?>
-				<td class="price">
-					<?=$product->optPricesArr[$sum] ? Currency::drawAllCurrenciesPrice($product->optPricesArr[$sum]) : ' -нет- '?>
-				</td>
-				<?endforeach;?>
-				
-			</tr>
-            <?php
-            $i++;
-            ?>
-			<?endforeach;?>
-		<?endforeach;?>
-	<?endforeach;?>
-	</table>
-	
-	<?=$MODEL['textAfter']->attrs['descr']?>
-	
+            <div class="row top limiter" >
+                <div class="col-md-6 logo-container" ><img src="/img/logo.png" /></div>
+                <div class="col-md-6 info-container"><?=$MODEL['textTableTop']->attrs['descr']?></div>
+            </div>
+
+
+            <div class="row limiter tbl">
+                <table class="price-tbl" style="width: 100%; ">
+                    <tr class="header">
+                        <th>Наименование</th>
+                        <th>Розница</th>
+                        <?foreach(ProductSimple::$optPrices as $sum=>$isShown):?>
+                            <?if(!$isShown)continue;?>
+                            <th><?=Funx::numberFormat($sum)?> $</th>
+                        <?endforeach?>
+                    </tr>
+                    <?foreach($MODEL['catalog'] as $cat):?>
+                        <tr class="header">
+                            <td class="cat-name" colspan="<?=count(ProductSimple::$optPrices)+1?>"><?=$cat->name?></td>
+                        </tr>
+                        <?foreach($cat->products as $name=>$products):?>
+                            <?foreach($products as $product):?>
+                                <?if(!$product->optPricesArr) continue;?>
+                                <tr>
+                                        <td class="product" >
+                                            <div class="name"><?=$product->name?></div>
+                                            <div class="doze">
+                                                <a href="<?=$product->url()?>" target="_blank" ><?=OptPrice::shortenDozeStr($product->inPackage)?></a>
+                                            </div>
+                                        </td>
+
+
+                                    <td class="price"><?=Currency::drawAllCurrenciesPrice($product->price)?></td>
+
+                                    <?foreach(ProductSimple::$optPrices as $sum=>$isShown):?>
+                                        <?if(!$isShown)continue;?>
+                                        <td class="price">
+                                            <?=$product->optPricesArr[$sum] ? Currency::drawAllCurrenciesPrice($product->optPricesArr[$sum]) : ' -нет- '?>
+                                        </td>
+                                    <?endforeach;?>
+
+                                </tr>
+                                <?php
+                                $i++;
+                                ?>
+                            <?endforeach;?>
+                        <?endforeach;?>
+                    <?endforeach;?>
+                </table>
+            </div>
+
+
+        </div>
+
+    </div>
 </div>
-</div>
-

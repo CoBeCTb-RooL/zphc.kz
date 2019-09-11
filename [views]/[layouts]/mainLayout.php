956fbf2745b2efab2cd2a45a38055864610c5288
CoBeCTb <tsop.tya@gmail.com>
2019-09-11 19:58:39 +0600

    * fix

--- [views]/[layouts]/mainLayout.php
+++ [views]/[layouts]/mainLayout.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- <meta name="viewport" content="width=device-width; initial-scale=1.0001; maximum-scale=1.0001; minimum-scale=1.0001; user-scalable=no; target-densityDpi=device-dpi" /> -->
-    <meta name="viewport" content="width=device-width; initial-scale=1.001; " />
+    <meta name="viewport" content="width=device-width; initial-scale=1.001; user-scalable=no " />
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     
     <meta name="yandex-verification" content="9bd771476bdf0f4b" />

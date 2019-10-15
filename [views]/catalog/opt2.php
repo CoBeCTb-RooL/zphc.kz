86304a9c41386fbf56896e05f6231c1ad43d17fd
CoBeCTb <tsop.tya@gmail.com>
2019-10-15 17:07:05 +0600

    * fix

--- [views]/catalog/opt2.php
+++ [views]/catalog/opt2.php
@@ -51,9 +51,11 @@ $productsDictJson = json_encode($productsDict);
 
     //  чиним долбаные селекты в долбаном айос
     // jQuery(function ($) {
-        if (navigator.userAgent.match('iPad|iPhone|iPod') != -1  ) {
+    setTimeout(function(){
+        if (navigator.userAgent.match('iPad|iPhone|iPod') != -1  )
             $('.quans-wrapper select').addClass('iOSselect'); // provide a class for iOS select box
-        }
+    }, 500)
+
     // });
 
 </script>
